a435fb967e9cc38fe933229fa4747375e486b611
spasibo-ra <6201285+spasibo-ra@users.noreply.github.com>
2024-11-16 23:57:08 +0200

    Initial commit

---
+++ test/cdk-ws-chat-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkWsChatApp from '../lib/cdk-ws-chat-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-ws-chat-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkWsChatApp.CdkWsChatAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
